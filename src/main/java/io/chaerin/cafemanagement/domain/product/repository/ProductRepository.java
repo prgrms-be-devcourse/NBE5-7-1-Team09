3cbda0d88e797032998a22ec759a3c3e58e2717d
dosaa7 <dosaa7@naver.com>
2025-04-24 08:13:54 +0900

    feat: DAO 클래스 생성

---
+++ src/main/java/io/chaerin/cafemanagement/domain/product/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package io.chaerin.cafemanagement.domain.product.repository;
+
+import io.chaerin.cafemanagement.domain.product.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
