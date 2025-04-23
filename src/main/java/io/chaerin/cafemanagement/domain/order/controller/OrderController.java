28cf2ac1be615a0c79af30197132dbb215df640e
JaeoneHeo <jaewonheo666@gmail.com>
2025-04-24 02:06:52 +0900

    feat: Controller에 주문 생성, 주문 수정을 추가한다

--- src/main/java/io/chaerin/cafemanagement/domain/order/controller/OrderController.java
+++ src/main/java/io/chaerin/cafemanagement/domain/order/controller/OrderController.java
@@ -1,5 +1,6 @@
 package io.chaerin.cafemanagement.domain.order.controller;
 
+import io.chaerin.cafemanagement.domain.order.dto.OrderUpdateRequestDto;
 import io.chaerin.cafemanagement.domain.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -11,14 +12,24 @@ import org.springframework.web.bind.annotation.*;
 public class OrderController {
     private final OrderService orderService;
 
+    @PostMapping
+    public ResponseEntity<Object> saveOrder(@RequestBody OrderUpdateRequestDto request) {
+        return ResponseEntity.ok(orderService.saveOrder(request));
+    }
+
     @GetMapping
     public ResponseEntity<Object> getOrdersByEmail(@RequestParam String email) {
         return ResponseEntity.ok(orderService.getOrdersByEmail(email));
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateOrder(@PathVariable Long id, @RequestBody OrderUpdateRequestDto request) {
+        return ResponseEntity.ok(orderService.updateOrder(id, request));
+    }
+
     @DeleteMapping("/{id}")
-    public ResponseEntity<Object> deleteOrder(@PathVariable Long orderId) {
-        orderService.deleteOrder(orderId);
+    public ResponseEntity<Object> deleteOrder(@PathVariable Long id) {
+        orderService.deleteOrder(id);
         return ResponseEntity.ok().build();
     }
 }
