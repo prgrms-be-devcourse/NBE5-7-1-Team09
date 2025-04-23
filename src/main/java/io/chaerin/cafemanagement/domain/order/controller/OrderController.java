2398102a4d6b10fc61fbf2073d7bcfd0416c48e2
JaeoneHeo <jaewonheo666@gmail.com>
2025-04-23 16:48:56 +0900

    feat: OrderController를 구현한다

---
+++ src/main/java/io/chaerin/cafemanagement/domain/order/controller/OrderController.java
@@ -0,0 +1,24 @@
+package io.chaerin.cafemanagement.domain.order.controller;
+
+import io.chaerin.cafemanagement.domain.order.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+
+    @GetMapping
+    public ResponseEntity<Object> getOrdersByEmail(@RequestParam String email) {
+        return ResponseEntity.ok(orderService.getOrdersByEmail(email));
+    }
+
+    @DeleteMapping("/{orderId}")
+    public ResponseEntity<Object> deleteOrder(@PathVariable Long orderId) {
+        orderService.deleteOrder(orderId);
+        return ResponseEntity.ok().build();
+    }
+}
