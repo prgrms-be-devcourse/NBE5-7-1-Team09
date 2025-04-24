2d71224402c9ed00b7e5312bc6934f4f12ee339b
min0962 <wsx8028@gmail.com>
2025-04-24 23:57:10 +0900

    fix: 컨트롤러 매핑 오류 수정

--- src/main/java/io/chaerin/cafemanagement/domain/question/controller/QuestionController.java
+++ src/main/java/io/chaerin/cafemanagement/domain/question/controller/QuestionController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @Controller
-@RequestMapping("question")
+@RequestMapping("order")
 @RequiredArgsConstructor
 public class QuestionController {
 
